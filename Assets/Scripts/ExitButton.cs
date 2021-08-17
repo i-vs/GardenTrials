626180549b01c4bebf317bd92a67a7ce567358a4
Ivana <ivanacramos@gmail.com>
2021-08-17 03:59:51 -0700

    Adding Exit button for Desktop

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ExitGarden()
+    {
+        Debug.Log("quitting");
+        Application.Quit();
+    }
+
+}
